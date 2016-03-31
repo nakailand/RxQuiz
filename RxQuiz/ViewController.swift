83eed84bd217f0dcc134b8fa41560644a092f70e
nakailand <shota_nakajima@dwango.co.jp>
2016-03-31 17:17:35 +0900

    first commit

---
+++ RxQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxQuiz
+//
+//  Created by nakazy on 2016/03/31.
+//  Copyright © 2016年 nakazy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
